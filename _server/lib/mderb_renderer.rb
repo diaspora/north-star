7db9975716979b21c90cc0ab4841a4d30488b4e0
Dennis Schubert <mail@dennis-schubert.de>
2018-10-27 08:41:35 +0200

    Expose front matter contents to erb

--- _server/lib/mderb_renderer.rb
+++ _server/lib/mderb_renderer.rb
@@ -4,21 +4,20 @@ module Sinatra
   module MderbRenderer
     def mderb(section, path)
       document = settings.storage.load(section, path)
-      render_mderb(document[:contents])
+      contents = render_markdown(document[:contents])
+
+      erb(
+        contents,
+        locals: {
+          page: document[:frontmatter]
+        }
+      )
     end
 
     private
 
-    def render_mderb(contents)
-      render_erb(render_markdown(contents))
-    end
-
     def render_markdown(contents)
       settings.markdown_renderer.render(contents)
-    end
-
-    def render_erb(contents)
-      erb(contents)
     end
   end
 
