f0a80b2c7575dc46c91302264628c11a9261d8d0
Dennis Schubert <mail@dennis-schubert.de>
2018-10-31 06:25:49 +0100

    Implement Redis-based filesystem cache for production

--- _server/lib/storage.rb
+++ _server/lib/storage.rb
@@ -2,6 +2,30 @@
 
 module Storage
   class Dummy
+    def load_document(section, path)
+      file_path = get_document_file_name(section, path, ::I18n.locale) unless ::I18n.locale == ::I18n.default_locale
+      file_path ||= get_document_file_name(section, path)
+      return false unless file_path
+
+      receive_document(file_path)
+    end
+
+    def get_document_file_name(section, path, translation=false)
+      root_path = if translation
+                    ::Helpers.path_expand_join(@paths[:translations], translation.to_s, "contents", section, path)
+                  else
+                    ::Helpers.path_expand_join(@paths[:contents], section, path)
+                  end
+      file_path = "#{root_path}.md"
+
+      unless file_exist?(file_path)
+        return false unless dir_exist?(root_path)
+
+        return get_document_file_name(section, "#{path}/index", translation)
+      end
+
+      file_path
+    end
   end
 
   require_relative "storage/file"
--- _server/lib/storage/redis.rb
+++ _server/lib/storage/redis.rb
@@ -4,6 +4,37 @@ module Storage
   class Redis < Dummy
     def initialize(paths)
       @paths = paths
+      @redis = ::Redis.new
+
+      import_contents
+    end
+
+    def receive_document(path)
+      contents = @redis.get(path)
+      JSON.parse(contents).deep_symbolize_keys
+    end
+
+    private
+
+    def import_contents
+      @redis.flushall
+
+      file_storage = Storage::File.new(@paths)
+      file_storage.list_documents.each do |document_path|
+        @redis.set(
+          document_path,
+          file_storage.receive_document(document_path).to_json
+        )
+      end
+    end
+
+    def file_exist?(path)
+      @redis.keys(path).present?
+    end
+
+    def dir_exist?(path)
+      path += "/" unless path[-1] == "/"
+      @redis.keys("#{path}*").present?
     end
   end
 end
